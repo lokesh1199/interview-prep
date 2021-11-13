ac15f43c3a62201b38bc9d3c2a5d3f0a8d509a05
lokesh1199 <lokesh9911@protonmail.com>
2021-11-13 20:35:26 +0530

    add files

--- array/008.py
+++ array/008.py
@@ -2,16 +2,14 @@
 
 def maxContigousSum(a: list):
     if not len(a):
-        return
+        return 0
     maxSum = a[0]
     for i in range(len(a)):
         for j in range(i+1, len(a)+1):
-            print(a[i:j])
             curSum = sum(a[i:j])
             maxSum = curSum if maxSum < curSum else maxSum
     return maxSum
 
 
-a = input('list1: ').split()
-a = [int(i) for i in a]
+a = list(map(int, input().split()))
 print(maxContigousSum(a))
